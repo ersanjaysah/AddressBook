1331c8b90b9113fd2baaa228dfbba80e3fdc4b82
sanjay <ssah6694@gmail.com>
2022-03-20 10:31:02 +0530

    [sanjay]Added new Contact Address in to a file

--- CAddressBookManagement/CAddressBookManagement/PersonsBook.cs
+++ CAddressBookManagement/CAddressBookManagement/PersonsBook.cs
@@ -8,8 +8,8 @@ using System.IO;
 
 namespace CAddressBookManagement
 {
-    public class PersonsBook
-    {
+   
+    
         public class Persons_Book
         {
             public string FirstName;   //variables
@@ -20,6 +20,37 @@ namespace CAddressBookManagement
             public long PhoneNumber;
             public String Email;
 
+            public string Result()
+            {
+                return "First Name : " + FirstName + "\nLast Name : " + LastName + "\nCity : " + City + "\nState : " + State + "\nZip/Pin : " + Zip + "\nPhoneNumber : " + PhoneNumber + "\nEmail_ID : " + Email + "\n*******************************";
+            }
+
         }
-    }
+
+        public class AddressBookManager
+        {
+            public List<Persons_Book> listName = new List<Persons_Book>();
+
+            public void AddPersons_Details(Persons_Book newPerson)
+            {
+                listName.Add(newPerson);
+                string adding_AllDataIn_JsonFile = JsonConvert.SerializeObject(listName);
+                File.WriteAllText(@"E:\BridgeLabzAssignment\AddressBook.json", adding_AllDataIn_JsonFile);
+            }
+
+            public AddressBookManager()
+            {
+                string inAllJsonContent = File.ReadAllText(@"E:\BridgeLabzAssignment\AddressBook.json");
+                if (inAllJsonContent.Length > 0)
+                {
+                    listName = JsonConvert.DeserializeObject<List<Persons_Book>>(inAllJsonContent);
+                }
+                else
+                {
+                    listName = new List<Persons_Book>();
+                }
+            }
+
+        }
+    
 }
--- CAddressBookManagement/CAddressBookManagement/Program.cs
+++ CAddressBookManagement/CAddressBookManagement/Program.cs
@@ -2,11 +2,47 @@
 
 namespace CAddressBookManagement
 {
-    internal class Program
+    public class Program
     {
         static void Main()
         {
             Console.WriteLine("\"Welcome To The Address Book Program\"\n");
+
+            AddressBookManager obj = new AddressBookManager();
+
+            Persons_Book obj1 = new Persons_Book();
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip/Pin");
+            int zip = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number");
+            long phoneNumber = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Email-ID");
+            string eMail = Console.ReadLine();
+
+            obj1.FirstName = firstName;
+            obj1.LastName = lastName;
+            obj1.City = city;
+            obj1.State = state;
+            obj1.Zip = zip;
+            obj1.PhoneNumber = phoneNumber;
+            obj1.Email = eMail;
+
+            obj.AddPersons_Details(obj1);
+
+
         }
     }
 }
